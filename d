ce95694721edb58867c592465da4d3d3a993adc8
Affan <affan_94@hotmail.com>
2020-02-26 15:44:07 +0500

    website page created

---
+++ d
@@ -0,0 +1,35 @@
+[33mcommit 96a7a25d9108cb595836887d3013f33653975c9f[m[33m ([m[1;36mHEAD -> [m[1;32mmaster[m[33m)[m
+Author: Affan <affan_94@hotmail.com>
+Date:   Wed Feb 26 12:36:54 2020 +0500
+
+    to test if things are updated
+
+[33mcommit 3d29deaab689621a6beba5ee70db83d2894e70a2[m[33m ([m[1;31morigin/master[m[33m)[m
+Author: Affan <affan_94@hotmail.com>
+Date:   Wed Feb 26 12:22:43 2020 +0500
+
+    recreated index after modifying gitignore file
+
+[33mcommit cff8c25cb38d77d8e4530cc9140b783df72f1d02[m
+Author: Affan <affan_94@hotmail.com>
+Date:   Tue Feb 25 23:45:52 2020 +0500
+
+    migrating project to local drive
+
+[33mcommit b4b9815374f928c1dd3bf2ae84270b84773e8325[m
+Author: affan <affan_94@hotmail.com>
+Date:   Mon Feb 24 23:07:41 2020 -0500
+
+    new 10years experience section added and logos added in logo about section
+
+[33mcommit 1c86767d9c660cce3bc6ffdd54e19617a70cafa9[m
+Author: affan <affan_94@hotmail.com>
+Date:   Sat Feb 22 21:10:17 2020 -0500
+
+    Home page updated with 3 packages
+
+[33mcommit ed4b57cd570315a0b99b48eb0e6595ce5cff310d[m
+Author: affan <affan_94@hotmail.com>
+Date:   Fri Feb 21 16:10:54 2020 -0500
+
+    Home Page Completed
---
+++ website.php
@@ -0,0 +1,93 @@
+<?php include "head.php"; ?>
+
+<title>Contact</title>
+</head>
+
+
+<body class="contact">
+    <div class="content-wrapper">
+        <?php include "header.php"; ?>
+        <div class="main-wrapper">
+            <main>
+
+                <section style="background-image: url('./images/inner-page-header.webp')" id="page-header-contact" class="page-header">
+                    <h1 class="abs-head">Contact Us</h1>
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-12">
+                                <h1>Get In Touch With Us</h1>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                
+                <section id="website-about" class="section">
+                    <div class="container">
+                        <div class="col-6">
+                            
+                        </div>
+                        <div class="col-6"></div>
+                    </div>
+                </section>
+
+                <section id="testimonials" class="section">
+                    <div class="container">
+                        <h3>What Our Client Says</h3>
+                        <div class="row">
+                            <div class="col-12">
+                                <!-- MAIN SLIDES -->
+                                <div class="slider">
+                                    <div data-index="1">
+                                        <blockquote>
+                                            Your text goes here. this is just placeholder content.Your text goes here. this is just placeholder content. your text goes here. Your text goes here. this is just placeholder content. your text goes here.
+                                        </blockquote>
+                                        <div class="author">L. Wilson</div>
+                                    </div>
+                                    <div data-index="2">
+                                        <blockquote>
+                                            Your text goes here. this is just placeholder content.Your text goes here. this is just placeholder content. your text goes here. Your text goes here. this is just placeholder content. your text goes here.
+                                        </blockquote>
+                                        <div class="author">L. Wilson</div>
+                                    </div>
+                                    <div data-index="3">
+                                        <blockquote>
+                                            Your text goes here. this is just placeholder content.Your text goes here. this is just placeholder content. your text goes here. Your text goes here. this is just placeholder content. your text goes here.
+                                        </blockquote>
+                                        <div class="author">L. Wilson</div>
+                                    </div>
+                                    <div data-index="4">
+                                        <blockquote>
+                                            Your text goes here. this is just placeholder content.Your text goes here. this is just placeholder content. your text goes here. Your text goes here. this is just placeholder content. your text goes here.
+                                        </blockquote>
+                                        <div class="author">L. Wilson</div>
+                                    </div>
+                                </div>
+
+                                <!-- THUMBNAILS -->
+                                <div class="slider-nav-thumbnails">
+                                    <div class="slick-current"><img src="images/testi-1.webp" slide="slide_1">
+                                    </div>
+                                    <div><img src="images/testi-2.webp" slide="slide_2">
+                                    </div>
+                                    <div><img src="images/testi-3.webp" slide="slide_3">
+                                    </div>
+                                    <div><img src="images/testi-4.webp" slide="slide_4">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+
+
+
+
+            </main>
+        </div>
+        <?php include "footer.php"; ?>
+        <?php include_once('popup-quote.php'); ?>
+
+    </div>
+
+    <?php include_once('libraries.php'); ?>
+</body>
