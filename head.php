982048bdb5fcc17a2d398c1a7fcdd86cf19fe255
Affan <affan_94@hotmail.com>
2020-03-17 21:39:21 +0500

    gtag and meta tags added in head section

--- head.php
+++ head.php
@@ -1,10 +1,20 @@
-<?php //header('Cache-Control: max-age=31536000'); ?>
-
-
+<?php header('Cache-Control: max-age=31536000'); ?>
 <!doctype html>
 <html lang="en">
 
 <head>
+
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-160825110-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-160825110-1');
+</script>
+
+
     <noscript>
         <style>
             /**
@@ -18,6 +28,47 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+
+    <meta name="author" content="Logo Brisk">
+    <meta name="description" content="Your business brand identity is what makes you stand out before everything like customers, competitors, and investors. Professional logo design will reflect your business obligations and commitments towards high quality work delivery.">
+
+
+
+<meta name="robots" content="max-snippet:-1, max-image-preview:large, max-video-preview:-1">
+
+<meta name="keywords" content="web de, website, crowdsource, graphic design, design, logo design, web development, graphic designer, web design company, design logo, logo design services, banner design, landing page design services, business card ,
+design,graphic, outstanding creative, cool, professional, photoshop, designer" />
+
+
+<meta name="apple-mobile-web-app-title" content="Logo Brisk" />
+
+
+<meta property="og:title" content="Logo Brisk - Professional logo design agency">
+
+<meta property="og:site_name" content="Logo Brisk">
+
+<meta property="og:url" content="https://logobrisk.com/">
+
+<meta property="og:description" content="Logo Brisk - Your business brand identity is what makes you stand out before everything like customers, competitors, and investors. Professional logo design will reflect your business obligations and commitments towards high quality work delivery.">
+
+<meta property="og:type" content="website">
+
+<meta property="og:image" content="https://logobrisk.com/images/logobrisk.png">
+
+
+<meta name="twitter:title" content="Logo Brisk - Professional logo design agency">
+
+
+<meta name="twitter:card" content="summary_large_image">
+
+<meta name="twitter:description" content="Logo Brisk - Your business brand identity is what makes you stand out before everything like customers, competitors, and investors. Professional logo design will reflect your business obligations and commitments towards high quality work delivery.">
+
+
+
+<meta name="twitter:image" content="https://logobrisk.com/images/logobrisk.png">
+
+
     <link rel="stylesheet" href="./css/bundeled.css?ver=1" async>
     <link rel="stylesheet" href="./css/swiper.css">
     <link rel="stylesheet" href="./css/style.css?ver=1" async>
